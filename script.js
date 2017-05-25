caef568b0bd47dddaf2a59066ec73bf69bd3c618
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-05-25 15:12:48 -0400

    add projects

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 $( document ).ready(function() {
 
   console.log('Ready!!')
-
-  $("#frontend").click(function()
-                       {
+  var aboveHeight = $('.topbackground').outerHeight();
+  console.log(aboveHeight)
+  
+  $("#frontend").click(function(){
     $("#frontskills").animate({width:'toggle'}, 400);
     $("#backskills").css({display: 'none'});
     $("#otherskills").css({display: 'none'});
     return false;
   });
-  $("#backend").click(function()
-                       {
+  $("#backend").click(function(){
     $("#backskills").animate({width:'toggle'}, 400);
     $("#frontskills").css({display: 'none'});
     $("#otherskills").css({display: 'none'});
     return false;
   });
-  $("#others").click(function()
-                       {
+  $("#others").click(function(){
     $("#otherskills").animate({width:'toggle'}, 400);
     $("#backskills").css({display: 'none'});
     $("#frontskills").css({display: 'none'});
     return false;
   });
+  
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > (aboveHeight - (0.10246082 * aboveHeight))){
+      console.log('top')
+      // if yes, add “fixed” class to the <nav>
+      // add padding top to the #content 
+      $('.navbar').addClass('fixed');
+    } else {
+      // when scroll up or less than aboveHeight,
+      console.log('bottom')
+      $('.navbar').removeClass('fixed');
+    }
+  });
+  
+  
+  
 });
