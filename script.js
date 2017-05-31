9b29e4414794136fa5e4a4988b1c7489c3bc763f
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-05-31 15:27:32 -0400

    add project scrolling

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   console.log('Ready!!')
   var aboveHeight = $('.topbackground').outerHeight();
   console.log(aboveHeight)
-  
+
   $("#frontend").hover(function(){
     $("#frontskills").animate({width:'toggle'}, 400);
     $("#backskills").css({display: 'none'});
@@ -22,20 +22,57 @@ $( document ).ready(function() {
     $("#frontskills").css({display: 'none'});
     return false;
   });
-  
+
   $(window).scroll(function(){
     if ($(window).scrollTop() > (aboveHeight - (0.10246082 * aboveHeight))){
-      console.log('top')
-      // if yes, add “fixed” class to the <nav>
-      // add padding top to the #content 
       $('.navbar').addClass('fixed');
     } else {
-      // when scroll up or less than aboveHeight,
-      console.log('bottom')
       $('.navbar').removeClass('fixed');
     }
   });
+
+
+  let projArray = $("#projects .projectlist")
+  let projectWindow = $("#projects")
+  let x=1;
+
+  $('#rightchev').click(function(){
+    if (x < 4){
+      x++
+      $('#project1').css({display:'none'})
+      $('#project2').css({display:'none'})
+      $('#project3').css({display:'none'})
+      $('#project4').css({display:'none'})
+      $('#project' + (x)).css({display: 'block'})
+    } else {
+      x = 0
+      x++
+      $('#project1').css({display:'none'})
+      $('#project2').css({display:'none'})
+      $('#project3').css({display:'none'})
+      $('#project4').css({display:'none'})
+      $('#project' + (x)).css({display: 'block'})
+    }
+  })
   
-  
-  
+  $('#leftchev').click(function(){
+    if (x > 1){
+      x--
+      $('#project1').css({display:'none'})
+      $('#project2').css({display:'none'})
+      $('#project3').css({display:'none'})
+      $('#project4').css({display:'none'})
+      $('#project' + (x)).css({display: 'block'})
+    } else {
+      x = 5
+      x--
+      $('#project1').css({display:'none'})
+      $('#project2').css({display:'none'})
+      $('#project3').css({display:'none'})
+      $('#project4').css({display:'none'})
+      $('#project' + (x)).css({display: 'block'})
+    }
+  })
+ 
+
 });
