99237842fd6df4a53e3bccdb4fca9fc0f461d146
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-09-03 14:24:05 -0400

    add project toggle

--- version_2/portfolio_site/src/App.js
+++ version_2/portfolio_site/src/App.js
@@ -6,13 +6,30 @@ import { Projects } from './projects';
 class App extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      centerView: "0"
+    }
+    this.centerViewPanel = this.centerViewPanel.bind(this);
+  };
+  
+  centerViewPanel(action){
+    this.state.centerView = action.target.value
   }
   
-  
   render() {
+    
+    
+    let renderCenter = [<Titleblock />, <Projects />]
+    
     return (
       <div className="App">
         <Titleblock />
+        <ul className="Navbar1">
+          <li onClick={this.centerViewPanel} value="1" >Skills </li>
+          <li onClick={this.centerViewPanel} value="2" >Projects</li>
+          <li onClick={this.centerViewPanel} value="3" >About Me</li>
+          <li onClick={this.centerViewPanel} value="0" >Home</li>
+        </ul>
         <Projects />
       </div>
     );
--- version_2/portfolio_site/src/projects.js
+++ version_2/portfolio_site/src/projects.js
@@ -6,20 +6,38 @@ export class Projects extends Component {
     super(props);
     this.state = {
       projects: [
-        {name: "GameShot", imageUrl: "./Images/gameshot.png", description: ""},
-        {name: "Brick Breaker", imageUrl: "./Images/brickbreakre.png", description: ""},
-      ]
+        {name: "GameShot", imageUrl: "./Images/gameshot.png", description: "1"},
+        {name: "Brick Breaker", imageUrl: "./Images/brickbreakre.png", description: "2"},
+      ],
+      projectIndex: 0
     }
+    this.clickRight = this.clickRight.bind(this)
   };
   
-  render(){
-    let nextProject = function() {
+  clickRight(action){
+    if(this.state.projectIndex < this.state.projects.length-1) {
+      this.state.projectIndex += 1
+      console.log(this.state.projectIndex)
+      console.log(this.state.projects[this.state.projectIndex])
       
+    } else {
+      this.state.projectIndex = 0
+      console.log(this.state.projectIndex)
+      console.log(this.state.projects[this.state.projectIndex])
     }
+  }
+  
+  render(){
+    let projectId = this.state.projectIndex
+    let projectrender = this.state.projects[projectId].name
+    
     return (
       <div id="projectsContainer">
         <h2>Project Title</h2>
-        <img src={nextProject} />
+        <h3 onClick={this.clickRight}>Next</h3>
+        <h2>{this.state.projectIndex}</h2>
+        <img src="" />
+        <h3>{projectrender}</h3>
       </div>
     );
   }
