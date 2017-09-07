b26ed2a8e54eb6c498dcf91d715812f45378e3ba
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-09-06 20:16:57 -0400

    fix website head name

--- src/aboutme.js
+++ src/aboutme.js
@@ -15,7 +15,7 @@ export class AboutMe extends Component {
           <p>
             My background in music and engineering supports me in approaching a problem using both facts and creativity.  I use logical thinking to combine both aspects and come to an effective solution.  I am always curious about the various routes to an answer, and value building relationships and receiving feedback from peers.
             <br/><br/>
-            I discovered my passion for programming and web development later in life, but that hasn't stopped me from persuing my passion.  Building these vast websites and apps joins my creativity and intuition in ways I've never experienced.  Because of this, my skills traverse both Front-end and Back-end development, and so does my enjoyment.
+            I discovered my passion for programming and web development later in life, but that hasn't stopped me from pursuing my passion.  Building these vast websites and apps joins my creativity and intuition in ways I've never experienced.  Because of this, my skills traverse both Front-end and Back-end development, and so does my enjoyment.
           </p>
             </div>
         </div>
