974af0278c58c7fee9a273e4500020612dd32400
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-10-08 21:57:14 -0400

    redesign layout of projects

--- src/projects.js
+++ src/projects.js
@@ -7,24 +7,24 @@ export class Projects extends Component {
     super(props);
     this.state = {
       projects: [
-        {name: "GameShot", imageUrl: "./Images/gameshot.png", description: ["Built using Rails and various Ruby gems", "based on functionality of Instagram (image posts, comments, followers, timeline) with an emphasis on PC Gamers"],
+        {name: "GameShot", imageUrl: "./Images/gameshot.png", description: ["Built using Rails and various Ruby gems.  Based on functionality of Instagram (image posts, comments, followers, timeline) with an emphasis towards PC Gamers.  AWS used to host images and display them on profiles and timeline."],
         technologies: ["Ruby on Rails","JavaScript","HTML/CSS","PostgreSQL","AWS S3","Paperclip","Devise","Ransack","HTTParty"],
         github: "https://github.com/RJHendricks22/gaming_instagram", website: "https://gameshot.herokuapp.com/"},
         {name: "Brick Breaker",
         imageUrl: "./Images/brickbreaker.png", 
-        description: ["Built in JavaScript, using HTML canvas and CSS", "Animation frames on canvas element allow ball velocity", "Created all hit detection functions from scratch"],
+        description: ["Built in JavaScript, using HTML canvas and CSS.  Animation frames on canvas element allows ball velocity, which increases over time.  Hit detection between elements was derived by hand."],
         technologies: ["JavaScript","HTML/CSS"], 
         github: "https://github.com/RJHendricks22/rjhendricks22.github.io", website: "https://rjhendricks22.github.io/"},
         {name: "Attr_Reader", imageUrl: "./Images/attrreader.png", 
-        description: ["Group project built in Rails","Users can request to trade books with other users", "Virtual Bookshelf rendered through ERB and HTML","Requested books send email with shipping info to book owner"],
+        description: ["Group project built in Rails where users can request to trade books with other users.  Virtual Bookshelf is rendered through ERB and HTML based on number of books available.  On request, an email is sent to book owner with shipping info of the book requester."],
         technologies: ["Ruby on Rails","JavaScript","HTML/CSS","GoogleBooks API","Ransack","Action Mailer","HTTParty","Devise"],
         github: "https://github.com/RJHendricks22/Flourish_and_Blotts",
         website: "http://attrreader3.herokuapp.com/"},
-        {name: "MagicTCG Card Builder", imageUrl: "./Images/magiccardbuilder.png", description: ["NodeJS Express App","Users create an account to customize their own magic trading card", "Complete cards saved to account profile"],
+        {name: "MagicTCG Card Builder", imageUrl: "./Images/magiccardbuilder.png", description: ["NodeJS Express App where users create an account to customize their own magic trading card.  Complete cards saved to account profile which can be downloaded as JPG.  API calls MagicTGio for examples.  Blank card backgrounds were made using Photoshop."],
         technologies: ["NodeJS","ExpressJS","JavaScript","MagicTheGathering.io API","HTML/CSS","B-Crypt","BodyParser","Multer","Mustache","Animate.css"], 
          github: "https://github.com/RJHendricks22/MagicTGCardBuilder", 
          website: "https://github.com/RJHendricks22/MagicTGCardBuilder"},
-        {name: "Custom Mad Libs Builder", imageUrl: "./Images/constructionimage.jpg", description: ["ReactJS app allowing users to generate custom MadLib", "Copy and past story, choose where blanks will go","Pick what part of speech each blank is","Render the story and play!"],
+        {name: "Custom Mad Libs Builder", imageUrl: "./Images/constructionimage.jpg", description: ["ReactJS app allowing users to generate custom MadLib.  Copy and paste story, then choose where the blanks will go.  Then pick what part of speech each blank is, render the story and play!"],
          technologies: ["ReactJS","JavaScript","HTML/CSS","Webpack","Babel.js"],
          github: "https://github.com/RJHendricks22/mad-libs-site",
          website: "https://github.com/RJHendricks22/mad-libs-site"}
@@ -70,20 +70,25 @@ export class Projects extends Component {
                                      
     let technologiesRender = currentProject.technologies.map( tech => <li>{tech}</li>);
     
+    let projectNavBar = this.state.projects.map( project => <li>{project.name}</li>);
+    
     return (
       <div id="myprojects">
         <div className="projectNavBar animated fadeIn">
-        
+          {projectNavBar}
         </div>
         <div className="projectsContainer animated fadeIn">
-            <h2>{currentProject.name}</h2>
             <img id="leftarrow" className="projectArrows" src={require("./Images/prev.png")} onClick={this.clickLeft} />
             <img id="rightarrow" className="projectArrows" src={require("./Images/next.png")} onClick={this.clickRight} />
             <a href={currentProject.website}><img src={require(`${currentProject.imageUrl}`)} className="projectImage"/></a>
             <a href={currentProject.github}><img id="githubproject" src={require("./Images/githubwhite.png")} /></a>
             <a href={currentProject.website}><img id="webproject" src={require("./Images/whiteglobe.png")} /></a>
-          <ul className="descriptionList">{descriptionRender}</ul>
-          <ul className="techList">{technologiesRender}</ul>
+            <div  className="description">
+              <h2>{currentProject.name}</h2>
+              <ul>{descriptionRender}</ul>
+              <h2>Languages/Frameworks</h2>
+              <ul className="techList">{technologiesRender}</ul>
+            </div>
         </div>
       </div>
     );
