63375161aedeafd98fdafccc131135b1d93c0aec
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-11-06 16:19:43 -0500

    changeup homepage

--- src/homepage.js
+++ src/homepage.js
@@ -3,12 +3,14 @@ import './App.css';
 import './animate.css';
 
 export class Homepage extends Component {
-  
+  // <img src={require("./Images/welcome1.png")} /> if i want the welcome picture
   render(){
     
     return(
       <div id="welcomePanel" className="animated2 fadeIn">
-        <img src={require("./Images/welcome1.png")} />
+        <h1 className="animated6 fadeIn animated">Hello, I'm Ray</h1>
+        <h2 className="animated7 fadeIn animated">I'm a Full Stack Web Developer</h2>
+        <h2 className="animated8 fadeIn animated">Please check out my work and links to my contact info! If you have any questions, email me at RayJHendricks22@gmail.com</h2>
       </div>
     );
   }
